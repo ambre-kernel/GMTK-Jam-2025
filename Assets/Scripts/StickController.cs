a21f144bed67b0b93178414080109156fa2b1715
ambre-kernel <ambre.kernel@outlook.com>
2025-07-30 23:57:15 +0200

    Mouse control

--- Assets/Scripts/StickController.cs
+++ Assets/Scripts/StickController.cs
@@ -2,10 +2,12 @@ using UnityEngine;
 
 public class StickController : MonoBehaviour
 {
+    [SerializeField] bool useMouse = false;
     [SerializeField] bool isLeftStick = true;
     [SerializeField] float deadZone = .5f;
 
     private Vector2 stickDir = Vector2.zero;
+    private Vector2 mousePos = Vector2.zero;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,17 +18,25 @@ public class StickController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isLeftStick)
+        if (useMouse)
         {
-            stickDir.x = Input.GetAxisRaw("Horizontal");
-            stickDir.y = Input.GetAxisRaw("Vertical");
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            transform.up = (Vector3)(mousePos - new Vector2(transform.position.x, transform.position.y));
         }
         else
         {
-            stickDir.x = Input.GetAxisRaw("Horizontal2");
-            stickDir.y = Input.GetAxisRaw("Vertical2");
+            if (isLeftStick)
+            {
+                stickDir.x = Input.GetAxisRaw("Horizontal");
+                stickDir.y = Input.GetAxisRaw("Vertical");
+            }
+            else
+            {
+                stickDir.x = Input.GetAxisRaw("Horizontal2");
+                stickDir.y = Input.GetAxisRaw("Vertical2");
+            }
+            if (stickDir.magnitude < deadZone) stickDir = Vector2.zero;
+            else transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(-stickDir.x, stickDir.y) * Mathf.Rad2Deg);
         }
-        if (stickDir.magnitude < deadZone) stickDir = Vector2.zero;
-        else transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(-stickDir.x, stickDir.y) * Mathf.Rad2Deg);
     }
 }
